623b64f60a9f2be29acf9fbf250bca91057d678f
Nate Finch <nate.finch@gmail.com>
2013-02-25 12:18:11 -0500

    use updated tree and make StringTree wrapper

--- main.go
+++ main.go
@@ -5,12 +5,49 @@ import (
 	"github.com/natefinch/tree"
 )
 
-type StringData []string
+func main() {
 
-func (s StringData) Less(val string) func(int) bool {
-	return func(i int) bool { return val < s[i] }
+	// Create a StringTree from the given slice data
+	st := Treeify([]string{"banana", "apple", "domino", "cat", "zebra", "monkey", "hippo"})
+	fmt.Println("Data:", st.Data)
+
+	// walk over the tree in order, printing each node
+	fmt.Print("Tree: [")
+	tree.Walk(st.Tree, func(t *tree.Tree) { fmt.Print(st.Data[t.Val] + " ") })
+	fmt.Println("]")
+
+	// add new a new item
+	st.Insert("pineapple")
+	fmt.Println("Data:", st.Data)
+
+	// print the tree again
+	fmt.Print("Tree: [")
+	tree.Walk(st.Tree, func(t *tree.Tree) { fmt.Print(st.Data[t.Val] + " ") })
+	fmt.Println("]")
+
+	// let's do a binary search for the item we just inserted
+	fmt.Printf("Tree contains pineapple: %v\n", st.Find("pineapple"))
+
+	// now search for something that doesn't exist
+	fmt.Printf("Tree contains XXXXX: %v\n", st.Find("XXXXX"))
 }
 
+// Sample code for making a new type of tree.
+
+// Treeify returns a new StringTree populated with the given data
+func Treeify(s []string) *StringTree {
+	st := &StringTree{StringData(s), tree.New()}
+	for i, val := range s {
+		st.Tree.Insert(i, st.Data.Cmp(val))
+	}
+	return st
+}
+
+// StringData is just a slice that implements a comparison function
+type StringData []string
+
+// Cmp returns a closure that will compare the given string with
+// the string located at an index of the underlying slice
 func (s StringData) Cmp(val string) func(int) int8 {
 
 	// this is just a standard string compare based on the runes
@@ -32,45 +69,25 @@ func (s StringData) Cmp(val string) func(int) int8 {
 	}
 }
 
-func main() {
+// StringTree just packages the data and the tree together
+type StringTree struct {
+	Data StringData
+	Tree *tree.Tree
+}
 
-	// StringData is the backing data for our binary tree (just a slice of strings)
-	s := StringData([]string{"banana", "apple", "domino", "cat", "zebra", "monkey", "hippo"})
-	fmt.Println("Data:", s)
+// Insert inserts the given string into the tree
+func (s *StringTree) Insert(val string) {
+	// add data to the backing slice
+	s.Data = append(s.Data, val)
 
-	t := tree.New()
+	// use the Tree's insert method, passing StringData's comparison closure
+	s.Tree.Insert(len(s.Data)-1, s.Data.Cmp(val))
+}
 
-	// insert using StringData's less function
-	for i, v := range s {
-		t.Insert(i, s.Less(v))
-	}
+// Find returns true if the string is in the tree
+func (s *StringTree) Find(val string) bool {
 
-	// walk over the tree in order, printing each node
-	fmt.Print("Tree: [")
-	tree.Walk(t, func(t *tree.Tree) { fmt.Print(s[t.Val] + " ") })
-	fmt.Println("]")
-
-	// add new a new item to the backing data
-	w := "pineapple"
-	s = append(s, w)
-
-	fmt.Println("Data:", s)
-
-	// now insert it into the right place in the tree
-	t.Insert(len(s)-1, s.Less(w))
-
-	// print the tree again
-	fmt.Print("Tree: [")
-	tree.Walk(t, func(t *tree.Tree) { fmt.Print(s[t.Val] + " ") })
-	fmt.Println("]")
-
-	// let's do a binary search for the item we just inserted
-	idx := t.Search(s.Cmp(w))
-
-	fmt.Printf("Tree contains %s: %v\n", w, idx != -1)
-
-	// now search for something that doesn't exist
-	idx = t.Search(s.Cmp("XXXXX"))
-	fmt.Printf("Tree contains XXXXX: %v\n", idx != -1)
-
+	// use the tree's Search function, passing StringData's comparison closure
+	// Search returns the index of the item in the backing data if it exists, or -1 if it doesn't exist
+	return s.Tree.Search(s.Data.Cmp(val)) != -1
 }
